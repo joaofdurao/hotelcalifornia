98d2bbce2dbcf95c5c865076f673cf67e3027f77
joaofdurao <devjoaofelipe.dlc@gmail.com>
2023-05-27 12:17:14 -0300

    Configurando H2

--- src/main/java/br/com/hotelcalifornia/model/Hotel.java
+++ src/main/java/br/com/hotelcalifornia/model/Hotel.java
@@ -1,6 +1,8 @@
 package br.com.hotelcalifornia.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
@@ -19,6 +21,7 @@ import lombok.Setter;
 public class Hotel {
     
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String endereco;
