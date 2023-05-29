c052be0b8f279249374911810a2993450bea03c8
joaofdurao <devjoaofelipe.dlc@gmail.com>
2023-05-29 18:53:27 -0300

    Criação do método Delete e configuração do H2.

--- src/main/java/br/com/hotelcalifornia/model/Hotel.java
+++ src/main/java/br/com/hotelcalifornia/model/Hotel.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,6 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 @Entity
+@Table(name = "TB_HOTEIS")
 
 public class Hotel {
     
